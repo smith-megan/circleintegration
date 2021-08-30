bb236dd83f3e2f76319ff6e9263bb68cb1f348f8
Megan <smith.megan.design@gmail.com>
2021-08-30 14:29:36 -0600

    adjusted index.html ref

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path')
 const app=(express())
 
 app.get("/", (req, res)=>{
-  res.sendFile(path.join(__dirname, "../index.html"))
+  res.sendFile(path.join(__dirname, "./index.html"))
 })
 
 const PORT = process.env.PORT || 5000
